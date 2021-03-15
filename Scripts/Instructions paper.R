47f015db6a1a366f833a27efb4151317719a30d6
Hanh Tong <hanh.tong@tap.work>
2021-03-14 19:27:48 -0700

    Minor edit

--- Scripts/Instructions paper.R
+++ Scripts/Instructions paper.R
@@ -1,4 +1,4 @@
-# R code for statistical analysis for "Instructions" paper (JESA)
+# R code for statistical analysis for "Instructions" paper
 # Author: David Freeman, Erik Kimbrough, Garrett Petersen, and Hanh Tong
 
 # This code is part of the statistical analysis for "Instructions" paper
